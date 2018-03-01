66964f5f4aa2caf65e2d96b379b8cf978bb983c0
Lachlan Page <lachlanpage97@gmail.com>
2018-03-01 16:50:39 +1030

    Update to include static body constructor

--- Olympus Game Engine/src/components/PhysicsComponent.cpp
+++ Olympus Game Engine/src/components/PhysicsComponent.cpp
@@ -5,7 +5,7 @@ PhysicsComponent::PhysicsComponent(glm::vec3 positionBody, btCollisionShape* col
 	collisionShape = collisionShapeType;
 	fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(positionBody.x, positionBody.y, positionBody.z)));
 	btScalar mass = objectMass;
-	btVector3 fallInertia(20, 0, 0);
+	btVector3 fallInertia(0, 0, 0);
 	collisionShape->calculateLocalInertia(mass, fallInertia);
 	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, collisionShape, fallInertia);
 	fallRigidBody = new btRigidBody(fallRigidBodyCI);
@@ -16,7 +16,7 @@ PhysicsComponent::PhysicsComponent(glm::vec3 positionBody, btCollisionShape* col
 	collisionShape = collisionShapeType;
 	fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(positionBody.x, positionBody.y, positionBody.z)));
 	btScalar mass = objectMass;
-	btVector3 fallInertia(20, 0, 0);
+	btVector3 fallInertia(0, 0, 0);
 	collisionShape->calculateLocalInertia(mass, fallInertia);
 	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, collisionShape, fallInertia);
 	fallRigidBody = new btRigidBody(fallRigidBodyCI);
@@ -24,6 +24,16 @@ PhysicsComponent::PhysicsComponent(glm::vec3 positionBody, btCollisionShape* col
 	btVector3 dir = btVector3(Camera::Instance()->getDirection().x, Camera::Instance()->getDirection().y, Camera::Instance()->getDirection().z) * force;
 	fallRigidBody->applyCentralImpulse(dir);
 }
+
+PhysicsComponent::PhysicsComponent(glm::vec3 positionBody, btCollisionShape* collisionShapeType) {
+	//creates a fixed/static body.
+	collisionShape = collisionShapeType;
+	fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(positionBody.x, positionBody.y, positionBody.z)));
+	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(0, fallMotionState, collisionShape, btVector3(0,0,0));
+	fallRigidBody = new btRigidBody(fallRigidBodyCI);
+}
+
+
 
 btRigidBody* PhysicsComponent::getBody() {
 	return fallRigidBody;
