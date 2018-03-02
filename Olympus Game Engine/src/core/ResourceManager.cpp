e5f0fad0c41bf2bbd8c699dbb6e53409869fbb95
Lachlan Page <lachlanpage97@gmail.com>
2018-03-02 10:56:01 +1030

    Update resourcemanager to detect NrComponents

--- Olympus Game Engine/src/core/ResourceManager.cpp
+++ Olympus Game Engine/src/core/ResourceManager.cpp
@@ -34,15 +34,24 @@ unsigned int ResourceManager::loadImage(std::string filename) {
 	if (!data) {
 		Logger::Instance()->write(stbi_failure_reason() + filename);
 		Logger::Instance()->write("Failed to load Image: " + filename);
-		std::cout << "IMAGE LOAdeD FAILED" << std::endl;
+		std::cout << "Image failed to load: " << filename<< std::endl;
 	}
 	else {
+		GLenum format;
+		if (nrChannels == 1)
+			format = GL_RED;
+		else if (nrChannels == 3)
+			format = GL_RGB;
+		else if (nrChannels == 4)
+			format = GL_RGBA;
+
 		unsigned int texture;
 		glGenTextures(1, &texture);
 		glBindTexture(GL_TEXTURE_2D, texture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0,format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 		Logger::Instance()->write("LOADED IMAGE: " + filename);
+		std::cout << "LOADED IMAGE: " << filename << std::endl;
 		stbi_image_free(data);
 		return texture;
 	}
