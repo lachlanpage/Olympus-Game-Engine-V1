dce5fd92fc773d3673b289d11cf63ec44b1128db
Lachlan Page <lachlanpage97@gmail.com>
2018-03-08 20:03:16 +1030

    Update model texture handling

--- Olympus Game Engine/src/components/Model.h
+++ Olympus Game Engine/src/components/Model.h
@@ -47,7 +47,7 @@ private:
 		mesh_id = 0;
 		// read file via ASSIMP
 		Assimp::Importer importer;
-		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace | aiProcess_GenNormals);
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace |aiProcess_GenNormals );
 		//error checking {if root node and scene are null then we got problems}
 		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
 			std::cout << "ASSIMP:: ERROR" << std::endl;
@@ -103,10 +103,13 @@ private:
 				// use models where a vertex can have multiple texture coordinates so we always take the first set (0).
 				vec.x = mesh->mTextureCoords[0][i].x;
 				vec.y = mesh->mTextureCoords[0][i].y;
+				//std::cout << vec.x << " " << vec.y << std::endl;
+
 				vertex.TexCoords = vec;
 			}
-			else
-				vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+			//else
+			//	vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+
 			// tangent
 			vector.x = mesh->mTangents[i].x;
 			vector.y = mesh->mTangents[i].y;
@@ -119,6 +122,7 @@ private:
 			vertex.Bitangent = vector;
 			vertices.push_back(vertex);
 		}
+	
 		// now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
 		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
 		{
@@ -129,35 +133,12 @@ private:
 		}
 		// process materials
 		aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-		// we assume a convention for sampler names in the shaders. Each diffuse texture should be named
-		// as 'texture_diffuseN' where N is a sequential number ranging from 1 to MAX_SAMPLER_NUMBER. 
-		// Same applies to other texture as the following list summarizes:
-		// diffuse: texture_diffuseN
-		// specular: texture_specularN
-		// normal: texture_normalN
-
-		// 1. diffuse maps
-		//std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
-		//textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
-		// 2. specular maps
-		//std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
-		//textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
-		// 3. normal maps
-		//std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
-		//textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
-		// 4. height maps
-		//std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
-		//textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
 
 		std::vector<unsigned int> diffuseTextures = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
 		std::vector<unsigned int> specularTextures = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
 		std::vector<unsigned int> normalTextures = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
 		std::vector<unsigned int> displacementTextures = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_displacement");
 
-		std::cout << displacementTextures.size() << std::endl;
-		//ResourceManager::Instance()->printTextures(); 
-		// return a mesh object created from the extracted mesh data
-		//return Mesh(vertices, indices, textures, mesh_id);
 		return Mesh(vertices, indices, diffuseTextures, specularTextures, normalTextures, displacementTextures, mesh_id);
 	}
 
@@ -176,55 +157,7 @@ private:
 		}	
 		return textures;
 	}
-	/*
-	std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName) {
-		std::vector<Texture> textures;
-		
-		for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
-		{
-			aiString str;
-			mat->GetTexture(type, i, &str);
-
-			std::string filename = std::string(str.C_Str());
-			filename = this->directory + '/' + filename;
-
-			//unsigned int texture = ResourceManager::Instance()->loadTexture(filename);
-			if (typeName == "texture_diffuse") {
-
-			}
-			else if (typeName == "texture_normal") {
-				
-			}
-			else if (typeName == "texture_specular") {
-
-			}
-
-			// check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
-			bool skip = false;
-			for (unsigned int j = 0; j < textures_loaded.size(); j++)
-			{
-				if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
-				{
-					textures.push_back(textures_loaded[j]);
-					skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
-					break;
-				}
-			}
-			if (!skip)
-			{   // if texture hasn't been loaded already, load it
-				Texture texture;
-				texture.id = TextureFromFile(str.C_Str(), this->directory, false);
-				texture.type = typeName;
-				texture.path = str.C_Str();
-				textures.push_back(texture);
-				textures_loaded.push_back(texture);  // store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
-			}
-		}
-		return textures;
-	}
-
-	*/
-
+	
 	unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma)
 	{
 		std::string filename = std::string(path);
