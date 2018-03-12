cd3ca4c065e60285756c007eef4b4b01314b96d6
Lachlan Page <lachlanpage97@gmail.com>
2018-03-12 13:55:55 +1030

    Update sphere graphics component for use with PBR tests

--- Olympus Game Engine/src/components/SphereGraphicsComponent.cpp
+++ Olympus Game Engine/src/components/SphereGraphicsComponent.cpp
@@ -1,36 +1,96 @@
 #include "SphereGraphicsComponent.h"
 
 
-SphereGraphicsComponent::SphereGraphicsComponent() {
-	m_shader = ResourceManager::Instance()->loadShader("src/shaders/cube.vs", "src/shaders/cube.fs");
+SphereGraphicsComponent::SphereGraphicsComponent(float metallic, float roughness) {
+	m_shader = ResourceManager::Instance()->loadShader("src/shaders/pbr.vs", "src/shaders/pbr.fs");
+	//m_shadowShader = ResourceManager::Instance()->loadShader("src/shaders/shadow.vs", "src/shaders/shadow.fs");
 
-	m_stacks = 40;
-	m_slices = 40;
-	//generate sphere vertices
-	int radius = 10;
-	for (int stack = 0; stack < 40; ++stack) {
-		for (int slice = 0; slice < m_slices; ++slice) {
-			float y = 2.0 * stack / m_stacks - 1.0;
-			float r = sqrt(1 - y*y);
-			float x = r * sin(2.0 * 3.1415 * slice / m_slices);
-			float z = r * cos(2.0 * 3.1415 * slice / m_slices);
-
-			vertices.push_back(radius*x);
-			vertices.push_back(radius*y);
-			vertices.push_back(radius*z);
-		}
-	}
+	//std::cout << metallic << " " << roughness << std::endl;
+	m_roughness = roughness;
+	m_metallic = metallic;
 
 	//Create the appropriate buffers for the cube
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	const unsigned int X_SEGMENTS = 64;
+	const unsigned int Y_SEGMENTS = 64;
+	const float PI = 3.14159265359;
+	for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+	{
+		for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+		{
+			float xSegment = (float)x / (float)X_SEGMENTS;
+			float ySegment = (float)y / (float)Y_SEGMENTS;
+			float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+			float yPos = std::cos(ySegment * PI);
+			float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+			positions.push_back(glm::vec3(xPos, yPos, zPos));
+			uv.push_back(glm::vec2(xSegment, ySegment));
+			normals.push_back(glm::vec3(xPos, yPos, zPos));
+		}
+	}
+
+	bool oddRow = false;
+	for (int y = 0; y < Y_SEGMENTS; ++y)
+	{
+		if (!oddRow) // even rows: y == 0, y == 2; and so on
+		{
+			for (int x = 0; x <= X_SEGMENTS; ++x)
+			{
+				indices.push_back(y       * (X_SEGMENTS + 1) + x);
+				indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+			}
+		}
+		else
+		{
+			for (int x = X_SEGMENTS; x >= 0; --x)
+			{
+				indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+				indices.push_back(y       * (X_SEGMENTS + 1) + x);
+			}
+		}
+		oddRow = !oddRow;
+	}
+	indexCount = indices.size();
+
+	std::vector<float> data;
+	for (int i = 0; i < positions.size(); ++i)
+	{
+		data.push_back(positions[i].x);
+		data.push_back(positions[i].y);
+		data.push_back(positions[i].z);
+		if (uv.size() > 0)
+		{
+			data.push_back(uv[i].x);
+			data.push_back(uv[i].y);
+		}
+		if (normals.size() > 0)
+		{
+			data.push_back(normals[i].x);
+			data.push_back(normals[i].y);
+			data.push_back(normals[i].z);
+		}
+	}
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), &vertices[0], GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+	float stride = (3 + 2 + 3) * sizeof(float);
 	glEnableVertexAttribArray(0);
-}
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+
+	}
 void SphereGraphicsComponent::update(Entity& entity) {
 	m_shader->use();
 	m_shader->setMat4("view", Camera::Instance()->getViewMatrix());
@@ -38,8 +98,15 @@ void SphereGraphicsComponent::update(Entity& entity) {
 	glm::mat4 model;
 	model = glm::translate(model, entity.getPosition());
 	m_shader->setMat4("model", model);
+	m_shader->setFloat("m_metallic", m_metallic);
+	m_shader->setFloat("m_roughness", m_roughness);
+	m_shader->setVec3("camPos", Camera::Instance()->getPosition());
 	glBindVertexArray(VAO);
-
-	sendToRenderer(GL_POINTS, 0, m_stacks*m_slices);
+	glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+	std::cout << m_roughness << " Mmet: " << m_metallic << std::endl;
+	//sendToRenderer(GL_POINTS, 0, m_stacks*m_slices);
 }
+
+void SphereGraphicsComponent::renderShadow(Entity& entity){}
+void SphereGraphicsComponent::postInit(Entity& entity){}
 
