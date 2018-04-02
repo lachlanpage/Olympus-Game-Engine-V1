ca8cb93b2ad83af1db8511c2d41e6411e78fe2d5
Lachlan Page <lachlanpage97@gmail.com>
2018-04-02 19:46:19 +0930

    Update directional light PBR

--- Olympus Game Engine/src/shaders/directional_light.fs
+++ Olympus Game Engine/src/shaders/directional_light.fs
@@ -17,27 +17,98 @@ uniform vec3 lightDirection;
 uniform vec3 cameraPosition;
 uniform vec3 lightColor;
 
+vec2 poissonDisk[4] = vec2[](
+  vec2( -0.94201624, -0.39906216 ),
+  vec2( 0.94558609, -0.76890725 ),
+  vec2( -0.094184101, -0.92938870 ),
+  vec2( 0.34495938, 0.29387760 )
+);
+
+float random(vec3 a, int b){
+	vec4 seed4 = vec4(a,b);
+	float dot_product = dot(seed4, vec4(12.9898,78.233,45.164,94.673));
+    return fract(sin(dot_product) * 43758.5453);
+}
+
+float shadowMapLookup(vec2 coords){
+	return texture(shadowTexture, coords).r;
+}
+
+float texture2DCompare(sampler2D depths, vec2 uv, float compare){
+    float depth = texture2D(depths, uv).r;
+    return step(compare, depth);
+}
+
+float texture2DShadowLerp(sampler2D depths, vec2 size, vec2 uv, float compare){
+    vec2 texelSize = vec2(1.0)/size;
+    vec2 f = fract(uv*size+0.5);
+    vec2 centroidUV = floor(uv*size+0.5)/size;
+
+    float lb = texture2DCompare(depths, centroidUV+texelSize*vec2(0.0, 0.0), compare);
+    float lt = texture2DCompare(depths, centroidUV+texelSize*vec2(0.0, 1.0), compare);
+    float rb = texture2DCompare(depths, centroidUV+texelSize*vec2(1.0, 0.0), compare);
+    float rt = texture2DCompare(depths, centroidUV+texelSize*vec2(1.0, 1.0), compare);
+    float a = mix(lb, lt, f.y);
+    float b = mix(rb, rt, f.y);
+    float c = mix(a, b, f.x);
+    return c;
+}
+
+float PCF(sampler2D depths, vec2 size, vec2 uv, float compare){
+    float result = 0.0;
+    for(int x=-1; x<=1; x++){
+        for(int y=-1; y<=1; y++){
+            vec2 off = vec2(x,y)/size;
+            result += texture2DShadowLerp(depths, size, uv+off, compare);
+        }
+    }
+    return result/9.0;
+}
+
 float shadowCalculation(vec4 fragPosLightSpace, vec3 norm){
 	//perform perspective division as bias matirx not used 
 	vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
 	projCoords = projCoords * 0.5 + 0.5 ;//converts to [0,1] from [-1,1] for texture coordinates 
 	
 	float bias = max(0.02 * (1.0 - dot(norm, lightDirection)), 0.006);
-	
 	float currentDepth = projCoords.z;
-
-	float shadow = 0.0;
-	vec2 texelSize = 1.0 / textureSize(shadowTexture,0);
+	float shadow = 1.0;
+	vec2 texelSize = 1.0/ textureSize(shadowTexture,0);
 	for(int x = -1; x <=1; ++x){
 		for(int y = -1; y <=1;++y){
-			float pcfDepth = texture(shadowTexture, projCoords.xy + vec2(x,y) * texelSize).r;
+			float pcfDepth = shadowMapLookup(projCoords.xy + vec2(x,y) * texelSize);
+			//float pcfDepth = texture(shadowTexture, projCoords.xy + vec2(x,y) * texelSize).r;
 			shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
 		}
 	}
+	
+	//vec2 pixelPos = projCoords.xy/texelSize.x + vec2(0.5);
+	//vec2 fracPart = fract(pixelPos);
+	//vec2 startTexel = (pixelPos - fracPart) * texelSize.x;
+		
+	//float blTexel = shadowMapLookup(startTexel);
+	//float brTexel = shadowMapLookup(startTexel + vec2(texelSize.x , 0.0));
+	//float tlTexel = shadowMapLookup(startTexel + vec2(0.0, texelSize.y));
+	//float trTexel = shadowMapLookup(startTexel + vec2(texelSize.x, texelSize.y));
 
+	//float mixA = mix(blTexel, tlTexel, fracPart.y);
+	//float mixB = mix(brTexel, trTexel, fracPart.y);
+
+	//float mixFinal = mix(mixA, mixB, fracPart.x);
+
+	//float mixFinal2 = mix(mixFinal, shadow/9.0, 0.8);
+	//float mixB = mix()
+
+	//int index = int(16.0 * random(gl_FragCoord.xyy, 2))%16;
+
+	//}
+	//return 1-mixFinal;
 	shadow /= 9.0;
 	return shadow;
+	//return mixFinal2;
+	//return 1-shadow;
 }
+
 
 void main(){
 	vec2 UV = UV2 * 0.5 + 0.5;
